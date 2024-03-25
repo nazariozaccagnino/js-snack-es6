3bd2d17e7782a66de33bc605d3ce4d93aec5f0ea
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-03-25 14:52:34 +0100

    third snack started with data array

--- script.js
+++ script.js
@@ -17,57 +17,93 @@ vipTable.forEach((element, index, array) => {
 */
 // Snack 2
 
-const students = [
+// const students = [
+//     {
+//         id : 213,
+//         Name : 'Marco della Rovere',
+//         Grade : 78
+//     },
+//     {
+//         id : 110,
+//         Name : 'Paola Cortellessa',
+//         Grade : 96
+//     },
+//     {
+//         id : 250,
+//         Name : 'Andrea Mantegna',
+//         Grade : 48
+//     },
+//     {
+//         id : 145,
+//         Name : 'Gaia Borromini',
+//         Grade : 74
+//     },
+//     {
+//         id : 196,
+//         Name : 'Luigi Grimaldello',
+//         Grade : 68
+//     },
+//     {
+//         id : 102,
+//         Name : 'Piero della Francesca',
+//         Grade : 50
+//     },
+//     {
+//         id : 120,
+//         Name : 'Francesca da Polenta',
+//         Grade : 84
+//     },
+// ]
+
+// const plates = students.map((element)=>{    
+//     return element.Name.toUpperCase()
+// })
+// console.log(plates)
+
+// const best = students.filter((element)=>{
+//     if(element.Grade >= 70)
+//     return element
+// }) 
+// console.log(best)
+
+// const bestId = students.filter((element)=>{
+//     if(element.Grade >= 70 && element.id >= 120)
+//     return element
+// }) 
+// console.log(bestId)
+
+
+
+// Snack 3
+
+/*
+Canyon Ultimate CF Slx 9.0, 6,7 Kg
+Rose X-Lite Six Disc, 6.9 Kg
+Sarto asola, 5,7 Kg
+Wilier Triestina Zero.6, 6,1
+
+*/
+
+const bikes = [
     {
-        id : 213,
-        Name : 'Marco della Rovere',
-        Grade : 78
+        brand : 'Canyon',
+        model : 'Ultimate CF',
+        weight : 6700,
     },
     {
-        id : 110,
-        Name : 'Paola Cortellessa',
-        Grade : 96
+        brand : 'Rose',
+        model : 'X-Lite Six',
+        weight : 6900,
     },
     {
-        id : 250,
-        Name : 'Andrea Mantegna',
-        Grade : 48
+        brand : 'Sarto',
+        model : 'asola',
+        weight : 5700,
     },
     {
-        id : 145,
-        Name : 'Gaia Borromini',
-        Grade : 74
-    },
-    {
-        id : 196,
-        Name : 'Luigi Grimaldello',
-        Grade : 68
-    },
-    {
-        id : 102,
-        Name : 'Piero della Francesca',
-        Grade : 50
-    },
-    {
-        id : 120,
-        Name : 'Francesca da Polenta',
-        Grade : 84
+        brand : 'Wilier',
+        model : 'Triestina',
+        weight : 6100,
     },
 ]
 
-const plates = students.map((element)=>{    
-    return element.Name.toUpperCase()
-})
-console.log(plates)
-
-const best = students.filter((element)=>{
-    if(element.Grade >= 70)
-    return element
-}) 
-console.log(best)
-
-const bestId = students.filter((element)=>{
-    if(element.Grade >= 70 && element.id >= 120)
-    return element
-}) 
-console.log(bestId)
